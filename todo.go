b774fa36e82d2664a80af4e106649cf2475ad59c
Gabor Javorszky <gabor@javorszky.co.uk>
2018-02-18 00:56:59 +0000

    Add basic routing, database migration

---
+++ todo.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db := initDB("storage.db")
+	migrate(db)
+
+	e := echo.New()
+
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+
+	e.Logger.Fatal(e.Start(":8090"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+	CREATE TABLE IF NOT EXISTS tasks(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR(191) NOT NULL
+	);
+	`
+
+	_, err := db.Exec(sql)
+
+	if err != nil {
+		panic(err)
+	}
+}
